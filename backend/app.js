a5f15a41dd405a4bd4e15b0d283b362fcc0c3ed8
Mobin <sdm241405@gmail.com>
2024-09-18 01:13:31 +0600

    added api key

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,32 @@
 const express = require('express');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const morgan = require('morgan');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+// app.use(morgan('combined'));
+app.use(helmet());
+
+const apiKeyMiddleware = (req, res, next) => {
+  const apiKey = req.headers['x-api-key'];
+
+  console.log(apiKey === process.env.API_KEY);
+
+  if (!apiKey || apiKey !== process.env.API_KEY) {
+    return res.status(403).json({ message: 'Forbidden: Invalid API Key' });
+  }
+  next();
+};
+app.use(apiKeyMiddleware);
 
 app.use('/', (req, res) => {
   res.json({ message: 'api is working' });
--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 
-export default nextConfig;
+const config = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        API_KEY: "9c035862-3350-4204-9c87-0a65d5c7c85f",
+      },
+    };
+  }
+
+  return {
+    env: {
+      API_KEY: "9c035862-3350-4204-9c87-0a65d5c7c85f",
+    },
+  };
+};
+
+export default config;
