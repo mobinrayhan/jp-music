74fab94d0f54c50249ff4cdc2c88bb08f84f0805
Mobin <sdm241405@gmail.com>
2024-09-18 01:26:37 +0600

    added api url

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,6 +5,7 @@ const config = (phase) => {
     return {
       env: {
         API_KEY: "9c035862-3350-4204-9c87-0a65d5c7c85f",
+        API_URL: "http://localhost:4000/",
       },
     };
   }
@@ -12,6 +13,7 @@ const config = (phase) => {
   return {
     env: {
       API_KEY: "9c035862-3350-4204-9c87-0a65d5c7c85f",
+      API_URL: "https://jp-music.onrender.com/",
     },
   };
 };
