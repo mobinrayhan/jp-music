381243541aaa1a154e0ead55f878e50d416be82a
Mobin <sdm241405@gmail.com>
2024-10-04 00:55:43 +0600

    added main domain for the image link of the app

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,17 +1,27 @@
 import "./globals.css";
 
+const baseUrl =
+  process.env.NODE_ENV === "production"
+    ? "https://soundei.com"
+    : "http://localhost:3000";
+
 export async function generateMetadata() {
   const title = "SoundEi - Your Ultimate Sound Library";
   const description =
     "SoundEi is a sound platform where users can upload, browse, and download high-quality audio clips and sound effects for creative projects. Join us today!";
-  const image = "/app-logo.png";
+  const keywords =
+    "Sound downloads, audio clips, sound effects, royalty-free sounds, music for content creators, downloadable audio, sound library, SoundEi";
+  const author = "James Prince";
+  const image = `${baseUrl}/app-logo.png`;
 
   return {
     title,
     description,
+    author,
+    keywords,
     type: "website",
     locale: "en_US",
-    url: "https://soundei.com",
+    url: baseUrl,
     openGraph: {
       title,
       description,
@@ -20,7 +30,7 @@ export async function generateMetadata() {
           url: image,
           width: 1200,
           height: 630,
-          alt: "soundei Logo",
+          alt: "SoundEi Image Link Preview Image",
         },
       ],
     },
@@ -32,7 +42,6 @@ export async function generateMetadata() {
     },
   };
 }
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
