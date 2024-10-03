44105747d365b28fd9f8f76894291c10334de196
Mobin <sdm241405@gmail.com>
2024-10-03 12:23:36 +0600

    added construction page

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,5 +1,14 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "SoundEi - Your Ultimate Sound Library",
+  description:
+    "SoundEi is a sound platform where users can upload, browse, and download high-quality audio clips and sound effects for creative projects. Join us today!",
+  keywords:
+    "Sound downloads, audio clips, sound effects, royalty-free sounds, music for content creators, downloadable audio, sound library, SoundEi",
+  author: "James Prince",
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+  return (
+    <section className="container mx-auto flex h-full flex-col items-center justify-center gap-4 p-4 text-center">
+      <h1 className="text-3xl">We&apos;re Building Something Amazing!</h1>
+      <p>
+        {" "}
+        Our website is currently under construction. We&apos;re working hard to
+        bring you an exciting new experience. Stay tuned for updates!
+      </p>
+
+      <p> Thank you for your patience and support!</p>
+
+      <em> &mdash; James Prince</em>
+    </section>
+  );
+}
