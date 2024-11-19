381461f22f562aa1d21d4e9f6f93cfe3a082f3f3
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-11-19 17:03:37 +0600

    added basic routing and demo auth

--- soundei-admin/src/App.jsx
+++ soundei-admin/src/App.jsx
@@ -1,5 +1,24 @@
 import AppLayout from "./ui/AppLayout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import ProtectedRoute from "./ui/ProtectedRoute.jsx";
 
 export default function App() {
-  return <AppLayout />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          element={
+            <ProtectedRoute>
+              <AppLayout />;
+            </ProtectedRoute>
+          }
+        >
+          <Route path={"/"} />
+          <Route path={"/hello"} element={<h1>hello form </h1>} />
+        </Route>
+        <Route path={"/login"} element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ soundei-admin/src/ui/ProtectedRoute.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+  const isLoggedIn = false;
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate("/login");
+    }
+  }, [isLoggedIn, navigate]);
+
+  if (isLoggedIn) return children;
+}
