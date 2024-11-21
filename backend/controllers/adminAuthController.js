c3d0e3b9767191aa9ed12440e9353c2a30fa80c6
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-11-21 18:52:50 +0600

    added admin page login route

--- backend/controllers/adminAuthController.js
+++ backend/controllers/adminAuthController.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const authModels = require('./../models/authModel');
+const jwt = require('jsonwebtoken');
 
 exports.createUserByAdmin = async function (req, res, next) {
   const { username, email, password, role } = req.body;
@@ -29,6 +30,38 @@ exports.createUserByAdmin = async function (req, res, next) {
     return res.json({ message: 'User created successfully.' });
   } catch (err) {
     err.statusCode = 400;
+    next(err);
+  }
+};
+
+exports.postLoginUser = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await authModels.findUser(email);
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      const error = new Error('Invalid credentials!');
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const token = jwt.sign(
+      { email: user.email, id: user._id.toString() },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '7d',
+      }
+    );
+
+    return res.status(200).json({
+      message: 'Login successful',
+      token,
+      userId: user._id.toString(),
+      isActive: user.isActive,
+    });
+  } catch (err) {
     next(err);
   }
 };
--- backend/routes/adminAuth.js
+++ backend/routes/adminAuth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminAuthController');
 const { body } = require('express-validator');
-const authController = require('../controllers/authController');
 
 router.post(
   '/create-account',
@@ -21,6 +20,15 @@ router.post(
     .notEmpty()
     .withMessage('Role is required.'),
   adminController.createUserByAdmin
+);
+
+router.post(
+  '/login',
+  body('email').isEmail().withMessage('Email is not valid.').normalizeEmail(),
+  body('password')
+    .isLength({ min: 6 })
+    .withMessage('Password must be at least 6 characters long.'),
+  adminController.postLoginUser
 );
 
 module.exports = router;
