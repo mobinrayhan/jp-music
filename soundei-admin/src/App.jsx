bb6d877cbbefe8fb8f94329aa6206ec3aa0878f0
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-11-21 13:52:04 +0600

    added react query to our site

--- soundei-admin/src/App.jsx
+++ soundei-admin/src/App.jsx
@@ -1,3 +1,5 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools/production";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AllAudios from "./pages/audios/AllAudios.jsx";
 import Categories from "./pages/audios/Categories.jsx";
@@ -15,41 +17,53 @@ import AppLayout from "./ui/AppLayout";
 import ProtectedRoute from "./ui/ProtectedRoute.jsx";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          element={
-            <ProtectedRoute>
-              <AppLayout />;
-            </ProtectedRoute>
-          }
-        >
-          <Route index element={<Navigate to={"/dashboard/overview"} />} />
-          <Route path="dashboard">
-            <Route path="overview" element={<h1>Hello overview</h1>} />
-          </Route>
-          <Route path="users">
-            <Route path="all" element={<AllUsers />} />
-            <Route path="roles" element={<RolesAndPermission />} />
-            <Route path="blocked" element={<BlockList />} />
-            <Route path="new" element={<CreateNewUser />} />
-          </Route>{" "}
-          <Route path="audio">
-            <Route path="all" element={<AllAudios />} />
-            <Route path="upload" element={<UploadAudios />} />
-            <Route path="categories" element={<Categories />} />
-          </Route>
-          <Route path={"settings"}>
-            <Route path="general" element={<General />} />
-            <Route path="payment" element={<Payments />} />
-            <Route path="notifications" element={<Notification />} />
-          </Route>
-        </Route>
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+      },
+    },
+  });
 
-        <Route path="*" element={<NotFound />} />
-        <Route path={"/login"} element={<Login />} />
-      </Routes>
-    </BrowserRouter>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />;
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<Navigate to={"/dashboard/overview"} />} />
+            <Route path="dashboard">
+              <Route path="overview" element={<h1>Hello overview</h1>} />
+            </Route>
+            <Route path="users">
+              <Route path="all" element={<AllUsers />} />
+              <Route path="roles" element={<RolesAndPermission />} />
+              <Route path="blocked" element={<BlockList />} />
+              <Route path="new" element={<CreateNewUser />} />
+            </Route>{" "}
+            <Route path="audio">
+              <Route path="all" element={<AllAudios />} />
+              <Route path="upload" element={<UploadAudios />} />
+              <Route path="categories" element={<Categories />} />
+            </Route>
+            <Route path={"settings"}>
+              <Route path="general" element={<General />} />
+              <Route path="payment" element={<Payments />} />
+              <Route path="notifications" element={<Notification />} />
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+          <Route path={"/login"} element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
