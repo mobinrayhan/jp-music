0cd73b105a8554af7240a17060cdcbb386247563
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-11-21 16:53:09 +0600

    added react hot toster

--- soundei-admin/src/features/auth/useCreateUser.js
+++ soundei-admin/src/features/auth/useCreateUser.js
@@ -1,11 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 import { createNewUser } from "../../services/apiAuth";
 
 export default function useCreateUser() {
   const { isPending, mutate: signup } = useMutation({
     mutationFn: createNewUser,
     onSuccess: (user) => {
-      console.log(user);
+      toast.success(user.message || "User Created Successfully!");
+    },
+    onError: ({ response }) => {
+      toast.error(response?.data?.message || "Something Went Wrong !");
     },
   });
 
