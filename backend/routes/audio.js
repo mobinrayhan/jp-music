97652c6b706fdf7e92f8831c338af044db53ad91
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-11-24 02:30:30 +0600

    added isUserExist middleware to the user route

--- backend/routes/audio.js
+++ backend/routes/audio.js
@@ -1,12 +1,18 @@
 const router = require('express').Router();
 const audioController = require('../controllers/audioController');
 const isAuth = require('../middleware/isAuth');
+const isExistUser = require('../middleware/isUserExist');
 
 // router.get('/all', audioController.getAllAudios);
 // router.get('/category/:category', audioController.getAudiosByCategory);
 router.get('/search/:category', audioController.getCategoryAudiosWithSearch);
 router.get('/newest', audioController.getNewestAudiosWithSearch);
-router.post('/download', isAuth, audioController.postDownloadAudio);
+router.post(
+  '/download',
+  isAuth,
+  isExistUser(),
+  audioController.postDownloadAudio
+);
 router.get('/:id', audioController.getAudioById);
 
 module.exports = router;
