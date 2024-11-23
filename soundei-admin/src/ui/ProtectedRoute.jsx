3d73e848633ade77d58a3f2008c3d9e228c66f3b
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-11-23 16:38:14 +0600

    added full auth to the admin panel

--- soundei-admin/src/ui/ProtectedRoute.jsx
+++ soundei-admin/src/ui/ProtectedRoute.jsx
@@ -1,16 +1,21 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useUser from "../features/auth/useUser";
+import { getToken } from "../lib/getToken";
+import FullPageSpinner from "./FullPageSpinner";
 
 export default function ProtectedRoute({ children }) {
   const { isPending, user } = useUser();
+  const token = getToken();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (!user?.isActive && !isPending) {
+    if ((!user?.isActive && !isPending) || !token) {
       navigate("/login");
     }
-  }, [isPending, user?.isActive, navigate]);
+  }, [isPending, user?.isActive, navigate, token]);
 
-  if (user) return children;
+  if (isPending) return <FullPageSpinner />;
+
+  if (user && token) return children;
 }
