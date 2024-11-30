aa2913b2a2d4ff11c3779c1607c1104331b9dadd
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-11-30 19:41:36 +0600

    added sorted by category option

--- soundei-admin/src/features/audios/AudioTableOperation.jsx
+++ soundei-admin/src/features/audios/AudioTableOperation.jsx
@@ -3,6 +3,8 @@ import SortBy from "../../ui/SortBy";
 const options = [
   { label: "Sort By Name (A-Z)", value: "name-asc" },
   { label: "Sort By Name (Z-A)", value: "name-desc" },
+  { label: "Sort By Category (A-Z)", value: "category-asc" },
+  { label: "Sort By Category (Z-A)", value: "category-desc" },
   { label: "Sort By Date (lower first)", value: "createdAt-asc" },
   { label: "Sort By Date (higher first)", value: "createdAt-desc" },
 ];
