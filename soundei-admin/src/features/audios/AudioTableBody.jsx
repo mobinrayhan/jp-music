e378d3ccdc5245fb08937b14a845a5424735a999
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-12-01 19:28:32 +0600

    refactor audio table code

--- soundei-admin/src/features/audios/AudioTableBody.jsx
+++ soundei-admin/src/features/audios/AudioTableBody.jsx
@@ -1,15 +1,46 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import { CiMenuKebab } from "react-icons/ci";
+import { FaEdit, FaTrash } from "react-icons/fa";
 import { useSearchParams } from "react-router-dom";
+import Backdrop from "../../ui/Backdrop";
+import OptionsWrapper from "../../ui/OptionsWrapper";
+import { MENU_POPUP_WIDTH } from "./AudioTableList";
+import EditAudioPopup from "./EditAudioPopup";
 
-export default function AudioTableBody({
-  audios,
-  onAddMenuPosition,
-  positionAudioId,
-}) {
+export default function AudioTableBody({ audios, positionAudioId }) {
   const [searchParams] = useSearchParams();
-  const sortBy = searchParams.get("sortBy") || "name-asc";
+  const [positionAudio, setPositionAudio] = useState(null);
+  const [position, setPosition] = useState(null);
+  const [isEditableAudio, setIsEditableAudio] = useState(null);
+
+  const handleAddMenuPosition = (event, audio) => {
+    event.stopPropagation();
+
+    if (positionAudio?._id === audio._id) {
+      setPositionAudio(null);
+      return;
+    } else {
+      const buttonRect = event.target.getBoundingClientRect();
+      setPosition({
+        top: buttonRect.bottom + window.scrollY, // Button bottom
+        left: buttonRect.left + window.scrollX - MENU_POPUP_WIDTH, // Button left
+      });
+      setPositionAudio(audio);
+    }
+  };
+
+  function handleOnEdit() {
+    // for removing scroll
+    document.body.style.overflow = "hidden";
+
+    setIsEditableAudio((prev) => (prev ? null : positionAudio));
+    console.log(isEditableAudio);
+    setPositionAudio(null);
+  }
 
   // SORTING AUDIOS - chat gpt and jonas 😝
+  const sortBy = searchParams.get("sortBy") || "name-asc";
   const [field, direction] = sortBy.split("-");
   const modifier = direction === "asc" ? 1 : -1;
   const sortedAudios = [...audios].sort((a, b) => {
@@ -26,38 +57,72 @@ export default function AudioTableBody({
   });
 
   return (
-    <tbody>
-      {sortedAudios.map(({ _id, name, category, keywords, ...rest }) => (
-        <tr key={_id} className="border-b border-slate-200 bg-white">
-          <th
-            scope="row"
-            className="0 whitespace-nowrap px-6 py-4 font-medium text-black"
-          >
-            {name}
-          </th>
-          <td className="px-6 py-4">WILL BE ADDED</td>
-          <td className="px-6 py-4">{category}</td>
-          <td className="px-6 py-4">
-            {keywords?.join(", ") || "No Keywords Found!"}
-          </td>
-          <td className="flex justify-center px-6 py-4">
-            <button
-              className={`border border-slate-200 p-2 transition-all duration-150 hover:bg-slate-100 ${positionAudioId === _id ? "border-slate-300 bg-slate-200" : ""}`}
-              onClick={(eve) =>
-                onAddMenuPosition(eve, {
-                  _id,
-                  name,
-                  category,
-                  keywords,
-                  ...rest,
-                })
-              }
+    <>
+      <tbody>
+        {sortedAudios.map(({ _id, name, category, keywords, ...rest }) => (
+          <tr key={_id} className="border-b border-slate-200 bg-white">
+            <th
+              scope="row"
+              className="0 whitespace-nowrap px-6 py-4 font-medium text-black"
             >
-              <CiMenuKebab size={20} />
-            </button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
+              {name}
+            </th>
+            <td className="px-6 py-4">WILL BE ADDED</td>
+            <td className="px-6 py-4">{category}</td>
+            <td className="px-6 py-4">
+              {keywords?.join(", ") || "No Keywords Found!"}
+            </td>
+            <td className="flex justify-center px-6 py-4">
+              <button
+                className={`border border-slate-200 p-2 transition-all duration-150 hover:bg-slate-100 ${positionAudioId === _id ? "border-slate-300 bg-slate-200" : ""}`}
+                onClick={(eve) =>
+                  handleAddMenuPosition(eve, {
+                    _id,
+                    name,
+                    category,
+                    keywords,
+                    ...rest,
+                  })
+                }
+              >
+                <CiMenuKebab size={20} />
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+
+      {positionAudio && (
+        <OptionsWrapper
+          position={position}
+          onClose={setPositionAudio.bind(null, null)}
+        >
+          <ul>
+            <li
+              className="flex cursor-pointer items-center gap-2 p-2 px-4 text-sm transition-all duration-200 hover:bg-slate-100"
+              onClick={handleOnEdit}
+            >
+              <FaEdit /> Edit
+            </li>
+            <li className="flex cursor-pointer items-center gap-2 p-2 px-4 text-sm transition-all duration-200 hover:bg-slate-100">
+              <FaTrash /> Delete
+            </li>
+          </ul>
+        </OptionsWrapper>
+      )}
+
+      {isEditableAudio &&
+        createPortal(
+          <>
+            {" "}
+            <Backdrop onClick={handleOnEdit} />
+            <EditAudioPopup
+              editableAudio={isEditableAudio}
+              onClose={handleOnEdit}
+            />
+          </>,
+          document.body,
+        )}
+    </>
   );
 }
--- soundei-admin/src/features/audios/AudioTableList.jsx
+++ soundei-admin/src/features/audios/AudioTableList.jsx
@@ -1,14 +1,8 @@
-import { useState } from "react";
-import { createPortal } from "react-dom";
-import { FaEdit, FaTrash } from "react-icons/fa";
 import { useSearchParams } from "react-router-dom";
-import Backdrop from "../../ui/Backdrop";
-import OptionsWrapper from "../../ui/OptionsWrapper";
 import Spinner from "../../ui/Spinner";
 import AudioTableBody from "./AudioTableBody";
 import AudioTableFooter from "./AudioTableFooter";
 import AudioTableHeader from "./AudioTableHeader";
-import EditAudioPopup from "./EditAudioPopup";
 import useAudios from "./useAudios";
 
 export const MAX_AUDIO_PER_PAGE = 10;
@@ -16,9 +10,6 @@ export const MENU_POPUP_WIDTH = 140;
 
 export default function AudioTableList() {
   const [searchParams, setSearchParams] = useSearchParams();
-  const [position, setPosition] = useState(null);
-  const [positionAudio, setPositionAudio] = useState(null);
-  const [isEditableAudio, setIsEditableAudio] = useState(null);
   const { audios, isPending, error } = useAudios();
 
   const maxAudiosParams = searchParams.get("maxAudios");
@@ -53,41 +44,12 @@ export default function AudioTableList() {
     setSearchParams(searchParams);
   }
 
-  const handleAddMenuPosition = (event, audio) => {
-    event.stopPropagation();
-
-    if (positionAudio?._id === audio._id) {
-      setPositionAudio(null);
-      return;
-    } else {
-      const buttonRect = event.target.getBoundingClientRect();
-      setPosition({
-        top: buttonRect.bottom + window.scrollY, // Button bottom
-        left: buttonRect.left + window.scrollX - MENU_POPUP_WIDTH, // Button left
-      });
-      setPositionAudio(audio);
-    }
-  };
-
-  function handleOnEdit() {
-    // for removing scroll
-    document.body.style.overflow = "hidden";
-
-    setIsEditableAudio((prev) => (prev ? null : positionAudio));
-    console.log(isEditableAudio);
-    setPositionAudio(null);
-  }
-
   return (
     <>
       <div className="relative mt-6 overflow-x-auto">
         <table className="w-full bg-white text-left text-sm text-black rtl:text-right">
           <AudioTableHeader />
-          <AudioTableBody
-            audios={audios.audios}
-            onAddMenuPosition={handleAddMenuPosition}
-            positionAudioId={positionAudio?._id}
-          />
+          <AudioTableBody audios={audios.audios} />
           <AudioTableFooter
             isPending={isPending}
             onHasMore={handleHasMore}
@@ -97,38 +59,6 @@ export default function AudioTableList() {
           />
         </table>
       </div>
-
-      {positionAudio && (
-        <OptionsWrapper
-          position={position}
-          onClose={setPositionAudio.bind(null, null)}
-        >
-          <ul>
-            <li
-              className="flex cursor-pointer items-center gap-2 p-2 px-4 text-sm transition-all duration-200 hover:bg-slate-100"
-              onClick={handleOnEdit}
-            >
-              <FaEdit /> Edit
-            </li>
-            <li className="flex cursor-pointer items-center gap-2 p-2 px-4 text-sm transition-all duration-200 hover:bg-slate-100">
-              <FaTrash /> Delete
-            </li>
-          </ul>
-        </OptionsWrapper>
-      )}
-
-      {isEditableAudio &&
-        createPortal(
-          <>
-            {" "}
-            <Backdrop onClick={handleOnEdit} />
-            <EditAudioPopup
-              editableAudio={isEditableAudio}
-              onClose={handleOnEdit}
-            />
-          </>,
-          document.body,
-        )}
     </>
   );
 }
