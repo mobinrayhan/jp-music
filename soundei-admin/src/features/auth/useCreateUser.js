87411e1ad9a89e2f01fe5004e9c80c4074cca0e1
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2024-12-17 14:39:07 +0600

    added revalidation users after creating new user

--- soundei-admin/src/features/auth/useCreateUser.js
+++ soundei-admin/src/features/auth/useCreateUser.js
@@ -1,12 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import { createNewUser } from "../../services/apiAuth";
 
 export default function useCreateUser() {
+  const queryClient = useQueryClient();
+
   const { isPending, mutate: signup } = useMutation({
     mutationFn: createNewUser,
     onSuccess: (user) => {
       toast.success(user.message || "User Created Successfully!");
+      queryClient.invalidateQueries("users");
     },
     onError: (err) => {
       toast.error(err?.message || "Something Went Wrong !");
