3da045611a95f0792909c43def18234b9615a884
Rayhan Uddin Mobin <158156914+mobinrayhan@users.noreply.github.com>
2025-01-19 01:23:06 +0600

    added account components

--- frontend/src/components/account/account-main.jsx
+++ frontend/src/components/account/account-main.jsx
@@ -5,75 +5,79 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
 
-export default async function AccountMain({ params, searchParams }) {
+export default async function AccountMain() {
   const session = await getServerSession(authOptions);
   const user = await fetchWithApiKey("/users/account-info", {
     jwt: session.jwt,
     next: { cache: "no-store" },
   });
 
-  return (
-    <section className="custom-container mt-6 px-8">
-      <header>
-        <article className="flex items-center gap-6">
-          <div className="relative h-28 w-28">
-            <Image
-              src={"/no-image.png"}
-              className="object-cover"
-              alt="No Image"
-              fill
-            />
-          </div>
+  try {
+    return (
+      <section className="custom-container mt-6 px-8">
+        <header>
+          <article className="flex items-center gap-6">
+            <div className="relative h-28 w-28">
+              <Image
+                src={"/no-image.png"}
+                className="object-cover"
+                alt="No Image"
+                fill
+              />
+            </div>
 
-          <div>
-            <h3>
-              <strong>{user.accountInfo.username}</strong>
-            </h3>
-            <p>{user.accountInfo.email}</p>
-          </div>
+            <div>
+              <h3>
+                <strong>{user.accountInfo.username}</strong>
+              </h3>
+              <p>{user.accountInfo.email}</p>
+            </div>
 
-          <Button className="ml-auto">Logout</Button>
+            <Button className="ml-auto">Logout</Button>
+          </article>
+        </header>
+
+        <article className="mt-12">
+          <h3>Hello {user.accountInfo.username}!</h3>
+          <ul
+            className="my-5 ml-10 flex flex-col gap-3"
+            style={{
+              listStyleType: "desc",
+            }}
+          >
+            <li>
+              You have downloaded{" "}
+              <Link
+                href={"/my-library/downloads"}
+                className="text-blue-500 transition-all duration-200 hover:underline"
+              >
+                <strong> {user.accountInfo.totalDownloads} audios.</strong>
+              </Link>
+            </li>
+            <li>
+              You have created
+              <Link
+                href={"/my-library/playlists"}
+                className="text-blue-500 transition-all duration-200 hover:underline"
+              >
+                <strong> {user.accountInfo.totalPlaylists} playlists.</strong>
+              </Link>
+            </li>
+            <li>
+              Your favorites include{" "}
+              <Link
+                href={"/my-library/favorites"}
+                className="text-blue-500 transition-all duration-200 hover:underline"
+              >
+                <strong> {user.accountInfo.totalFavorites} items.</strong>
+              </Link>
+            </li>
+          </ul>
+          Enjoy your music journey! 🎶
         </article>
-      </header>
-
-      <article className="mt-12">
-        <h3>Hello {user.accountInfo.username}!</h3>
-        <ul
-          className="my-5 ml-10 flex flex-col gap-3"
-          style={{
-            listStyleType: "desc",
-          }}
-        >
-          <li>
-            You have downloaded{" "}
-            <Link
-              href={"/my-library/downloads"}
-              className="text-blue-500 transition-all duration-200 hover:underline"
-            >
-              <strong> {user.accountInfo.totalDownloads} audios.</strong>
-            </Link>
-          </li>
-          <li>
-            You have created
-            <Link
-              href={"/my-library/playlists"}
-              className="text-blue-500 transition-all duration-200 hover:underline"
-            >
-              <strong> {user.accountInfo.totalPlaylists} playlists.</strong>
-            </Link>
-          </li>
-          <li>
-            Your favorites include{" "}
-            <Link
-              href={"/my-library/favorites"}
-              className="text-blue-500 transition-all duration-200 hover:underline"
-            >
-              <strong> {user.accountInfo.totalFavorites} items.</strong>
-            </Link>
-          </li>
-        </ul>
-        Enjoy your music journey! 🎶
-      </article>
-    </section>
-  );
+      </section>
+    );
+  } catch (error) {
+    return <p>{error?.message || "Something went wrong !"}</p>;
+  }
 }
